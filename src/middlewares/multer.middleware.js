f2d881836ecb46b7a21f7873e20bada132455440
Prakhar <prakhar.5779.aps3@gmail.com>
2024-05-22 20:39:30 +0530

    File UPload using multer and cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+/*a middleware for file upload functionality of multer*/
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage: storage });
